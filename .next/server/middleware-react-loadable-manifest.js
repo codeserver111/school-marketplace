beb3d144f31eba4dc53195b093ee0fdcd1d02d4a
pappukrs <digitalpappu101@gmail.com>
2025-12-29 19:23:07 +0530

    Refactor admission flow UI and enhance document upload experience. Improve layout and usability of the admissions section, ensuring clarity in document requirements and streamlining user interactions.

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> @/components/SchoolMapView\":{\"id\":\"app/page.tsx -> @/components/SchoolMapView\",\"files\":[\"static/css/_app-pages-browser_src_components_SchoolMapView_tsx.css\",\"static/chunks/_app-pages-browser_src_components_SchoolMapView_tsx.js\"]}}"
